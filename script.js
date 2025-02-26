de987ee13c0b5f99073dd135d5d185d376eae110
An1106Q <huynhnguyenhoaian17022007@gmail.com>
2025-02-26 09:14:32 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+funtion login()
+{
+  
+  let a = username.value
+  let b = password.value
+  if (a =="Zzbilunzz" and b =="hoaian1976")
+  {
+    window.open"Home.html"
+    document.getElementById(compiler).style.display="block";
+  }
+  else
+    document.write("không hợp lệ")
+}
